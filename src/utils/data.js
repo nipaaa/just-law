a223388ac3e79ad27f483aab78d5205063d667a9
nipaaa <akternipa941@gmail.com>
2024-08-07 17:08:23 +0600

    footer add

--- src/utils/data.js
+++ src/utils/data.js
@@ -20,3 +20,97 @@ export const navitems = [
         path: "/blog"
     },
 ]
+
+export const footerData = [
+    {
+        title: "Company ",
+        menus: [
+            {
+                name: "Home",
+                path: "/"
+            },
+            {
+                name: "Practice Area",
+                path: "/practice-area"
+            },
+            {
+                name: "About Us",
+                path: "/about-us"
+            },
+            {
+                name: "Our Team",
+                path: "/our-team"
+            },
+            {
+                name: "Blog",
+                path: "/blog"
+            },
+        ]
+    },
+    {
+        title: "Services",
+        menus: [
+            {
+                name: "Bank & Financial",
+                path: "/"
+            },
+            {
+                name: "Car & MotorcycleAccidents",
+                path: "/"
+            },
+            {
+                name: "Family Law",
+                path: "/"
+            },
+            {
+                name: "Capital Markets",
+                path: "/"
+            },
+            {
+                name: "House & Estate Litigation",
+                path: "/"
+            },
+            {
+                name: "Criminal Law",
+                path: "/"
+            },
+        ]
+    },
+    {
+        title: "Quick Links",
+        menus: [
+            {
+                name: "News & Articles",
+                path: "/"
+            },
+            {
+                name: "Faq",
+                path: "/"
+            },
+            {
+                name: "Case Study",
+                path: "/"
+            },
+
+        ]
+    }
+]
+
+export const footerBottomData = [
+    {
+        title: "New York",
+        phone: " +44 7700185082",
+        email: " hello@yoursite.com"
+    },
+    {
+        title: "Toromto",
+        phone: " +44 7700185082",
+        email: " hello@yoursite.com"
+    },
+    {
+        title: "Las Vegas",
+        phone: " +44 7700185082",
+        email: " hello@yoursite.com"
+    },
+
+]
