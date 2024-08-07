01668901d79a2c2968d2f89f8dcdc7f51458a5de
nipaaa <akternipa941@gmail.com>
2024-08-07 13:11:54 +0600

    header done

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
+import { navitems } from '../../utils/data'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container">
+        <div className='header_inner'>
+          <Link to="/"><img src={logo} alt="" /></Link>
+          <div className='navitems'>
+            {
+              navitems.map((data, index) => <Link className='navitem' key={index} to={data.path}>{data.name}</Link>)
+            }
+          </div>
+          <div>
+            <button>Contact Us</button>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const navitems = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "Practice Areas",
+        path: "/practice-areas"
+    },
+    {
+        name: "About Us",
+        path: "/about-us"
+    },
+    {
+        name: "Our Team",
+        path: "/our-team"
+    },
+    {
+        name: "Blog",
+        path: "/blog"
+    },
+]
